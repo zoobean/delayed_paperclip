832d09f68c897d72bcf497b9a09975153ce575c5
Neil Cowburn <neilco@gmail.com>
2013-11-14 00:35:58 +0000

    Fixed failing tests

--- lib/delayed_paperclip/jobs/delayed_job.rb
+++ lib/delayed_paperclip/jobs/delayed_job.rb
@@ -4,13 +4,13 @@ module DelayedPaperclip
   module Jobs
     class DelayedJob < Struct.new(:instance_klass, :instance_id, :attachment_name)
 
-      if defined?(::Delayed::DeserializationError) # this is available in newer versions of DelayedJob. Using the newee Job api thus.
+      if Gem.loaded_specs['delayed_job'].version >= Gem::Version.new("2.1.0") # this is available in newer versions of DelayedJob. Using the newee Job api thus.
 
         def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
           ::Delayed::Job.enqueue(
             :payload_object => new(instance_klass, instance_id, attachment_name),
-            :priority => instance_klass.constantize.attachment_definitions[attachment_name][:delayed][:priority].to_i,
-            :queue => instance_klass.constantize.attachment_definitions[attachment_name][:delayed][:queue]
+            :priority => instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:priority].to_i,
+            :queue => instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:queue]
           )
         end
 
@@ -19,8 +19,8 @@ module DelayedPaperclip
         def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
           ::Delayed::Job.enqueue(
             new(instance_klass, instance_id, attachment_name),
-            instance_klass.constantize.attachment_definitions[attachment_name][:delayed][:priority].to_i,
-            instance_klass.constantize.attachment_definitions[attachment_name][:delayed][:queue]
+            instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:priority].to_i,
+            instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:queue]
           )
         end
 
